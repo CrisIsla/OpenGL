dbefa91f55310a82df84dd7199de801fae3cb316
Cristobal Isla <cris.isla98@gmail.com>
2022-12-30 12:11:31 -0300

    Add vertices data and create VBO.

---
+++ Projects/Triangle/Triangle/Triangle.cpp
@@ -0,0 +1,86 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); // this function is used to resize the window whenever the user resizes it.
+
+void processInput(GLFWwindow* window); // this function checks if a key is being press.
+
+int main()
+{
+	glfwInit(); // In order to use glfw, it has to be initialized.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // First argument is the option we want to configure, and the second is the value we want to set to it.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // In this case, we set the minor and major OpenGL version to 3, so it only works with this range of versions.
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Then, we set the OpenGL profile to the core profile.
+
+	// After the initialization, we need to create a window object.
+	GLFWwindow* window = glfwCreateWindow(800, 600, "HelloWindow!", NULL, NULL); // A window of 800 x 600 named 'HelloWindow!'
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // We make this window our current context.
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // We want to initialize GLAD before using any OpenGL function.
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600); // This function tells OpenGL the parameters of the window, in this case, we say start from the origin to 800 x 600.
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // In case the window is resized by the user, it resizes the image displayed too.
+
+	// NEW CODE STARTS HERE
+
+	float vertices[] = { // We define the coordinate of the three vertices of the triangle.
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO); // We generate a Vertex Buffer Object (VBO), that is used to store large amounts of data, like vertices.
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // We bind the buffer object we created to a ARRAY BUFFER object.
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // So here we are copying the vertices[] data into the buffer's memory.
+
+	// Now, to procces the triangle data, we have to create a vertex shader and a fragment shader.
+
+
+
+	// Now, every time we call GL_ARRAY_BUFFER object, we are using the VBO we just created (that is binded to the array buffer objecta)
+
+
+	// NEW CODE ENDS HERE
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window); // Check if ESCAPE key is being press. If so, it closes the window.
+
+		// Rendering commands should be added here
+		glClearColor(0.2f, 0.5f, 0.3f, 1.0f); // Clears the screen with the color specified.
+		glClear(GL_COLOR_BUFFER_BIT); // GL_COLOR_BUFFER_BIT makes it so it clears the screen with the color specified in glClearColor function. This function has two other options.
+
+		// Check and call events and swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
